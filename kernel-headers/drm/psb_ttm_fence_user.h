21a4d844b3ac96f2b9bb722f89c7bb561e904867
Jason Riordan <jriordan001@gmail.com>
2016-08-03 21:41:09 -0400

    Update kernel headers

--- kernel-headers/drm/psb_ttm_fence_user.h
+++ kernel-headers/drm/psb_ttm_fence_user.h
@@ -1,71 +1,140 @@
-/****************************************************************************
- ****************************************************************************
- ***
- ***   This header was automatically generated from a Linux kernel header
- ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
- ***   structures, and macros generated from the original header, and thus,
- ***   contains no copyrightable information.
- ***
- ***   To edit the content of this header, modify the corresponding
- ***   source file (e.g. under external/kernel-headers/original/) then
- ***   run bionic/libc/kernel/tools/update_all.py
- ***
- ***   Any manual change here will be lost the next time this script will
- ***   be run. You've been warned!
- ***
- ****************************************************************************
- ****************************************************************************/
+/**************************************************************************
+ *
+ * Copyright 2006-2008 Tungsten Graphics, Inc., Cedar Park, TX., USA
+ * All Rights Reserved.
+ * Copyright (c) 2009 VMware, Inc., Palo Alto, CA., USA
+ * All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ **************************************************************************/
+/*
+ * Authors
+ * Thomas Hellström <thomas-at-tungstengraphics-dot-com>
+ */
+
 #ifndef TTM_FENCE_USER_H
 #define TTM_FENCE_USER_H
-#ifndef _KERNEL
+
+#if !defined(__KERNEL__) && !defined(_KERNEL)
 #include <stdint.h>
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
 #endif
+
 #define TTM_FENCE_MAJOR 0
 #define TTM_FENCE_MINOR 1
-#define TTM_FENCE_PL 0
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define TTM_FENCE_DATE "080819"
+#define TTM_FENCE_PL    0
+#define TTM_FENCE_DATE  "080819"
+
+/**
+ * struct ttm_fence_signaled_req
+ *
+ * @handle: Handle to the fence object. Input.
+ *
+ * @fence_type: Fence types we want to flush. Input.
+ *
+ * @flush: Boolean. Flush the indicated fence_types. Input.
+ *
+ * Argument to the TTM_FENCE_SIGNALED ioctl.
+ */
+
 struct ttm_fence_signaled_req {
- uint32_t handle;
- uint32_t fence_type;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- int32_t flush;
- uint32_t pad64;
+	uint32_t handle;
+	uint32_t fence_type;
+	int32_t flush;
+	uint32_t pad64;
 };
+
+/**
+ * struct ttm_fence_rep
+ *
+ * @signaled_types: Fence type that has signaled.
+ *
+ * @fence_error: Command execution error.
+ * Hardware errors that are consequences of the execution
+ * of the command stream preceding the fence are reported
+ * here.
+ *
+ * Output argument to the TTM_FENCE_SIGNALED and
+ * TTM_FENCE_FINISH ioctls.
+ */
+
 struct ttm_fence_rep {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- uint32_t signaled_types;
- uint32_t fence_error;
+	uint32_t signaled_types;
+	uint32_t fence_error;
 };
+
 union ttm_fence_signaled_arg {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- struct ttm_fence_signaled_req req;
- struct ttm_fence_rep rep;
+	struct ttm_fence_signaled_req req;
+	struct ttm_fence_rep rep;
 };
-#define TTM_FENCE_FINISH_MODE_LAZY (1 << 0)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+
+/*
+ * Waiting mode flags for the TTM_FENCE_FINISH ioctl.
+ *
+ * TTM_FENCE_FINISH_MODE_LAZY: Allow for sleeps during polling
+ * wait.
+ *
+ * TTM_FENCE_FINISH_MODE_NO_BLOCK: Don't block waiting for GPU,
+ * but return -EBUSY if the buffer is busy.
+ */
+
+#define TTM_FENCE_FINISH_MODE_LAZY     (1 << 0)
 #define TTM_FENCE_FINISH_MODE_NO_BLOCK (1 << 1)
+
+/**
+ * struct ttm_fence_finish_req
+ *
+ * @handle: Handle to the fence object. Input.
+ *
+ * @fence_type: Fence types we want to finish.
+ *
+ * @mode: Wait mode.
+ *
+ * Input to the TTM_FENCE_FINISH ioctl.
+ */
+
 struct ttm_fence_finish_req {
- uint32_t handle;
- uint32_t fence_type;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- uint32_t mode;
- uint32_t pad64;
+	uint32_t handle;
+	uint32_t fence_type;
+	uint32_t mode;
+	uint32_t pad64;
 };
+
 union ttm_fence_finish_arg {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- struct ttm_fence_finish_req req;
- struct ttm_fence_rep rep;
+	struct ttm_fence_finish_req req;
+	struct ttm_fence_rep rep;
 };
+
+/**
+ * struct ttm_fence_unref_arg
+ *
+ * @handle: Handle to the fence object.
+ *
+ * Argument to the TTM_FENCE_UNREF ioctl.
+ */
+
 struct ttm_fence_unref_arg {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- uint32_t handle;
- uint32_t pad64;
+	uint32_t handle;
+	uint32_t pad64;
 };
+
+/*
+ * Ioctl offsets frome extenstion start.
+ */
+
 #define TTM_FENCE_SIGNALED 0x01
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define TTM_FENCE_FINISH 0x02
-#define TTM_FENCE_UNREF 0x03
+#define TTM_FENCE_FINISH   0x02
+#define TTM_FENCE_UNREF    0x03
+
 #endif
--- kernel-headers/linux/kct.h
+++ kernel-headers/linux/kct.h
@@ -1,98 +1,327 @@
-/****************************************************************************
- ****************************************************************************
- ***
- ***   This header was automatically generated from a Linux kernel header
- ***   of the same name, to make information necessary for userspace to
- ***   call into the kernel available to libc.  It contains only constants,
- ***   structures, and macros generated from the original header, and thus,
- ***   contains no copyrightable information.
- ***
- ***   To edit the content of this header, modify the corresponding
- ***   source file (e.g. under external/kernel-headers/original/) then
- ***   run bionic/libc/kernel/tools/update_all.py
- ***
- ***   Any manual change here will be lost the next time this script will
- ***   be run. You've been warned!
- ***
- ****************************************************************************
- ****************************************************************************/
 #ifndef KCT_H_
-#define KCT_H_
-#include <linux/netlink.h>
-#define EV_FLAGS_PRIORITY_LOW (1<<0)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#ifndef MAX_SB_N
-#define MAX_SB_N 32
-#endif
-#ifndef MAX_EV_N
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define MAX_EV_N 32
-#endif
-#define NETLINK_CRASHTOOL 27
-#define ATTCHMT_ALIGN 4U
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+#  define KCT_H_
+
+#  include <linux/netlink.h>
+/*
+ * warning: structures and constants in this header must match the
+ * ones in libc/kernel/common/linux/kct.h, so that information can
+ * be exchange between kernel and userspace throught netlink socket.
+ */
+/* flags to optionally filter events on android property activation */
+#define	EV_FLAGS_PRIORITY_LOW	(1<<0)
+
+#  ifndef MAX_SB_N
+#    define MAX_SB_N 32
+#  endif
+
+#  ifndef MAX_EV_N
+#    define MAX_EV_N 32
+#  endif
+
+#  define NETLINK_CRASHTOOL 27
+#  define ATTCHMT_ALIGN 4U
+
+/* Type of events supported by crashtool */
 enum ct_ev_type {
- CT_EV_STAT,
- CT_EV_INFO,
- CT_EV_ERROR,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- CT_EV_CRASH,
- CT_EV_LAST
+	CT_EV_STAT,
+	CT_EV_INFO,
+	CT_EV_ERROR,
+	CT_EV_CRASH,
+	CT_EV_LAST
 };
+
 enum ct_attchmt_type {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- CT_ATTCHMT_DATA0,
- CT_ATTCHMT_DATA1,
- CT_ATTCHMT_DATA2,
- CT_ATTCHMT_DATA3,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- CT_ATTCHMT_DATA4,
- CT_ATTCHMT_DATA5,
- CT_ATTCHMT_BINARY,
- CT_ATTCHMT_FILELIST,
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- CT_ATTCHMT_ADDITIONAL
+	CT_ATTCHMT_DATA0,
+	CT_ATTCHMT_DATA1,
+	CT_ATTCHMT_DATA2,
+	CT_ATTCHMT_DATA3,
+	CT_ATTCHMT_DATA4,
+	CT_ATTCHMT_DATA5,
+	/* Always add new types after DATA5 */
+	CT_ATTCHMT_BINARY,
+	CT_ATTCHMT_FILELIST,
+	CT_ATTCHMT_ADDITIONAL
 };
-#define CT_ADDITIONAL_NONE 0
-#define CT_ADDITIONAL_APLOG (1<<0)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define CT_ADDITIONAL_LAST_KMSG (1<<1)
-#define CT_ADDITIONAL_FWMSG (1<<2)
+
+#define CT_ADDITIONAL_NONE         0
+#define CT_ADDITIONAL_APLOG        (1<<0)
+#define CT_ADDITIONAL_LAST_KMSG    (1<<1)
+#define CT_ADDITIONAL_FWMSG        (1<<2)
+
 struct ct_attchmt {
- __u32 size;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- enum ct_attchmt_type type;
- char data[];
+	__u32 size; /* sizeof(data) */
+	enum ct_attchmt_type type;
+	char data[];
 } __aligned(4);
+
 struct ct_event {
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- __u64 timestamp;
- char submitter_name[MAX_SB_N];
- char ev_name[MAX_EV_N];
- enum ct_ev_type type;
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
- __u32 attchmt_size;
- __u32 flags;
- struct ct_attchmt attachments[];
+	__u64 timestamp;
+	char submitter_name[MAX_SB_N];
+	char ev_name[MAX_EV_N];
+	enum ct_ev_type type;
+	__u32 attchmt_size; /* sizeof(all_attachments inc. padding) */
+	__u32 flags;
+	struct ct_attchmt attachments[];
 } __aligned(4);
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+
 enum kct_nlmsg_type {
- KCT_EVENT,
- KCT_SET_PID = 4200,
+	/* kernel -> userland */
+	KCT_EVENT,
+	/* userland -> kernel */
+	KCT_SET_PID = 4200,
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+
 struct kct_packet {
- struct nlmsghdr nlh;
- struct ct_event event;
+	struct nlmsghdr nlh;
+	struct ct_event event;
 };
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define ATTCHMT_ALIGNMENT 4
-#ifndef KCT_ALIGN
-#define __KCT_ALIGN_MASK(x, mask) (((x) + (mask)) & ~(mask))
-#define __KCT_ALIGN(x, a) __KCT_ALIGN_MASK(x, (typeof(x))(a) - 1)
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
-#define KCT_ALIGN(x, a) __KCT_ALIGN((x), (a))
-#endif
-#define foreach_attchmt(Event, Attchmt)   if ((Event)->attchmt_size)   for ((Attchmt) = (Event)->attachments;   (Attchmt) < (typeof(Attchmt))(((char *)   (Event)->attachments) +   (Event)->attchmt_size);   (Attchmt) = (typeof(Attchmt))KCT_ALIGN(((size_t)(Attchmt))   + sizeof(*(Attchmt)) +   (Attchmt)->size, ATTCHMT_ALIGNMENT))
-#endif
-/* WARNING: DO NOT EDIT, AUTO-GENERATED CODE - SEE TOP FOR INSTRUCTIONS */
+
+#  define ATTCHMT_ALIGNMENT	4
+
+#  ifndef KCT_ALIGN
+#    define __KCT_ALIGN_MASK(x, mask)    (((x) + (mask)) & ~(mask))
+#    define __KCT_ALIGN(x, a)            __KCT_ALIGN_MASK(x, (typeof(x))(a) - 1)
+#    define KCT_ALIGN(x, a)		     __KCT_ALIGN((x), (a))
+#  endif /* !KCT_ALIGN */
+
+#  define foreach_attchmt(Event, Attchmt)				\
+	if ((Event)->attchmt_size)					\
+		for ((Attchmt) = (Event)->attachments;			\
+		     (Attchmt) < (typeof(Attchmt))(((char *)		\
+				  (Event)->attachments) +               \
+			(Event)->attchmt_size);                         \
+	(Attchmt) = (typeof(Attchmt))KCT_ALIGN(((size_t)(Attchmt)) \
+						     + sizeof(*(Attchmt)) + \
+			      (Attchmt)->size, ATTCHMT_ALIGNMENT))
+
+
+
+#ifdef __KERNEL__
+/*
+ * User should use the macros below rather than those extern functions
+ * directly. Laters' declaration are only to set them __weak so
+ * that the macros works fine.
+ */
+/* Raw API (deprecated) */
+extern struct ct_event *kct_alloc_event(const char *submitter_name,
+					const char *ev_name,
+					enum ct_ev_type ev_type,
+					gfp_t flags, uint eflags) __weak;
+extern int kct_add_attchmt(struct ct_event **ev,
+			   enum ct_attchmt_type at_type,
+			   unsigned int size,
+			   char *data, gfp_t flags)  __weak;
+extern void kct_free_event(struct ct_event *ev) __weak;
+extern int kct_log_event(struct ct_event *ev, gfp_t flags) __weak;
+
+/* API */
+#define MKFN(fn, ...) MKFN_N(fn, ##__VA_ARGS__, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)(__VA_ARGS__)
+#define MKFN_N(fn, n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n, ...) fn##n
+#define kct_log(...) MKFN(__kct_log_, ##__VA_ARGS__)
+
+#define __kct_log_4(Type, Submitter_name, Ev_name, flags) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+#define __kct_log_5(Type, Submitter_name, Ev_name, flags, Data0) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+#define __kct_log_6(Type, Submitter_name, Ev_name, flags, Data0, Data1) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+#define __kct_log_7(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+#define __kct_log_8(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					Data3) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+	#define __kct_log_9(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					 Data3, Data4) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			if (Data4) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA4, \
+					strlen(Data4) + 1, Data4, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+	#define __kct_log_10(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					 Data3, Data4, Data5) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			if (Data4) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA4, \
+					strlen(Data4) + 1, Data4, GFP_ATOMIC); \
+			if (Data5) \
+				kct_add_attchmt(&__ev, CT_ATTCHMT_DATA5, \
+					strlen(Data5) + 1, Data5, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+	#define __kct_log_11(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					 Data3, Data4, Data5, filelist) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			if (Data0) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			if (Data4) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA4, \
+					strlen(Data4) + 1, Data4, GFP_ATOMIC); \
+			if (Data5) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA5, \
+					strlen(Data5) + 1, Data5, GFP_ATOMIC); \
+			if (filelist) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_FILELIST, \
+					strlen(filelist) + 1, filelist, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+
+	#define __kct_log_12(Type, Submitter_name, Ev_name, flags, Data0, Data1, Data2, \
+					 Data3, Data4, Data5, filelist, add_steps) \
+	do {  if (kct_alloc_event) {	\
+		struct ct_event *__ev =	\
+			kct_alloc_event(Submitter_name, Ev_name, Type, \
+				GFP_ATOMIC, flags); \
+		if (__ev) { \
+			uint32_t add_steps_32 = add_steps;\
+			if (Data0) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA0, \
+					strlen(Data0) + 1, Data0, GFP_ATOMIC); \
+			if (Data1) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA1, \
+					strlen(Data1) + 1, Data1, GFP_ATOMIC); \
+			if (Data2) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA2, \
+					strlen(Data2) + 1, Data2, GFP_ATOMIC); \
+			if (Data3) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA3, \
+					strlen(Data3) + 1, Data3, GFP_ATOMIC); \
+			if (Data4) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA4, \
+					strlen(Data4) + 1, Data4, GFP_ATOMIC); \
+			if (Data5) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_DATA5, \
+					strlen(Data5) + 1, Data5, GFP_ATOMIC); \
+			if (filelist) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_FILELIST, \
+					strlen(filelist) + 1, filelist, GFP_ATOMIC); \
+			if (add_steps) \
+			kct_add_attchmt(&__ev, CT_ATTCHMT_ADDITIONAL, \
+					sizeof(uint32_t), \
+					(char *) &add_steps_32, GFP_ATOMIC); \
+			kct_log_event(__ev, GFP_ATOMIC); \
+		} \
+	} } while (0)
+#endif /*__KERNEL__*/
+#endif /* !KCT_H_ */
