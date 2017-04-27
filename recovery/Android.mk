2f6a1b64aeef10719aaafe681c37c827c65ef7af
Jason Riordan <jriordan001@gmail.com>
2017-04-26 22:21:58 -0400

    Z00A: update for 7.1, support decrypt

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -4,7 +4,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := librecovery_updater_mofd
-LOCAL_SRC_FILES := recovery_updater.c fw_version_check.c
+LOCAL_SRC_FILES := recovery_updater.cpp fw_version_check.cpp
 LOCAL_C_INCLUDES += bootable/recovery
 
 include $(BUILD_STATIC_LIBRARY)
